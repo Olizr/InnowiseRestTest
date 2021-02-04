d501fa0ffcccff45f47d41f0f182494f0dd1046b
Oliz <akyma54@mail.ru>
2021-02-04 13:45:12 +0300

    Models done

---
+++ src/main/java/olizarovich/probation/rest/models/Document.java
@@ -0,0 +1,177 @@
+package olizarovich.probation.rest.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * The Document class is data class for holding information
+ */
+public class Document {
+
+    /**
+     * Integer field. Contains primary key in class
+     */
+    private int id;
+
+    /**
+     * String field. Contains title of the document
+     */
+    private String title;
+
+    /**
+     * String field. Contains status of the document
+     */
+    private String status;
+
+    /**
+     * LocalDate field. Contains creation date of the document
+     * Format "yyyy-MM-dd"
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate creationDate;
+
+    /**
+     * LocalDate field. Contains deadline of the document
+     * Format "yyyy-MM-dd"
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate executionPeriod;
+
+    /**
+     * Integer field. Contains foreign key for customer
+     */
+    private int customerId;
+
+    /**
+     * Person type field. Contains customer
+     */
+    private Person customer;
+
+    /**
+     * Integer field. Contains foreign key for executor
+     */
+    private int executorId;
+
+    /**
+     * Person type field. Contains executo
+     */
+    private Person executor;
+
+    /**
+     * Empty class constructor.
+     */
+    public Document() {
+
+    }
+
+    /**
+     * Class constructor with all fields, except customer and executor
+     */
+    public Document(int id, String title, String status, LocalDate creationDate, LocalDate executionPeriod, int customerId, int executorId) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.executionPeriod = executionPeriod;
+        this.customerId = customerId;
+        this.executorId = executorId;
+    }
+
+    /**
+     * Uses id, document title, creationDate to compare objects
+     * @param o Object to compare with
+     * @return True if equals, false in other case
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Document document = (Document) o;
+        return id == document.id &&
+                Objects.equals(title, document.title) &&
+                Objects.equals(creationDate, document.creationDate);
+    }
+
+    /**
+     * Uses id, document title, creationDate to create hash
+     * @return Object hash code
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, creationDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getExecutionPeriod() {
+        return executionPeriod;
+    }
+
+    public void setExecutionPeriod(LocalDate executionPeriod) {
+        this.executionPeriod = executionPeriod;
+    }
+
+    public Person getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Person customer) {
+        this.customer = customer;
+    }
+
+    public Person getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(Person executor) {
+        this.executor = executor;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getExecutorId() {
+        return executorId;
+    }
+
+    public void setExecutorId(int executorId) {
+        this.executorId = executorId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/olizarovich/probation/rest/models/Person.java
@@ -0,0 +1,111 @@
+package olizarovich.probation.rest.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+* The Person class is data class for holding information
+*/
+public class Person {
+
+    /**
+     * Integer field. Contains primary key in class
+     */
+    private int id;
+
+    /**
+     * String field. Contains person first name
+     */
+    private String firstName;
+
+    /**
+     * String field. Contains person last name
+     */
+    private String lastName;
+
+    /**
+     * LocalDate field. Contains person birth date
+     * Format "yyyy-MM-dd"
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+    /**
+     * Empty class constructor.
+     */
+    public Person() {
+
+    }
+
+    /**
+     * Class constructor with all fields
+     * @param id Integer primary key
+     * @param firstName String first name
+     * @param lastName String last name
+     * @param birthDate LocalDate birth date
+     */
+    public Person(int id, String firstName, String lastName, LocalDate birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * Uses id, first name and last name to compare objects
+     * @param o Object to compare with
+     * @return True if equals, false in other case
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName);
+    }
+
+    /**
+     * Uses id, first name and last name to create hash
+     * @return Object hash code
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
